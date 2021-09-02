b85b755c7fe5abb7c1233a815dbd4aece7cc2a6c
Bastien PIPEREL <bastienpiperel@yahoo.fr>
2021-09-02 15:47:25 +0200

    gestion des evenements

--- src/Controller/admin/TypeEventController.php
+++ src/Controller/admin/TypeEventController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\admin;
 
 use App\Entity\TypeEvent;
 use App\Form\TypeEventType;
@@ -10,13 +10,13 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/type/event')]
+#[Route('admin/type/event')]
 class TypeEventController extends AbstractController
 {
     #[Route('/', name: 'type_event_index', methods: ['GET'])]
     public function index(TypeEventRepository $typeEventRepository): Response
     {
-        return $this->render('type_event/index.html.twig', [
+        return $this->render('admin/type_event/index.html.twig', [
             'type_events' => $typeEventRepository->findAll(),
         ]);
     }
@@ -36,17 +36,9 @@ class TypeEventController extends AbstractController
             return $this->redirectToRoute('type_event_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('type_event/new.html.twig', [
+        return $this->renderForm('admin/type_event/new.html.twig', [
             'type_event' => $typeEvent,
             'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'type_event_show', methods: ['GET'])]
-    public function show(TypeEvent $typeEvent): Response
-    {
-        return $this->render('type_event/show.html.twig', [
-            'type_event' => $typeEvent,
         ]);
     }
 
@@ -62,7 +54,7 @@ class TypeEventController extends AbstractController
             return $this->redirectToRoute('type_event_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('type_event/edit.html.twig', [
+        return $this->renderForm('admin/type_event/edit.html.twig', [
             'type_event' => $typeEvent,
             'form' => $form,
         ]);
--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -3,22 +3,35 @@
 namespace App\Form;
 
 use App\Entity\Event;
+use App\Entity\Intervenant;
+use App\Entity\TypeEvent;
+use PhpParser\Node\Stmt\Label;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class EventType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('start')
-            ->add('end')
-            ->add('maxcandidate')
+            ->add('typeEvent',  EntityType::class, [
+                'required' => false, 'label' => "type d'evenement", 'class' => TypeEvent::class, 'choice_label' => 'title',
+                'group_by' => function ($choice) {
+                    return $choice->getType();
+                },
+            ])
+
+            ->add('start', DateTimeType::class, ['date_widget' => 'single_text', 'label' => 'début'])
+            ->add('end', DateTimeType::class, ['date_widget' => 'single_text', 'label' => 'fin'])
+            ->add('maxcandidate', IntegerType::class, ['label' => "nombre de candidats maximum"])
             ->add('all_day')
-            ->add('intervenant')
-            ->add('typeEvent')
-        ;
+            ->add('intervenant', EntityType::class, ['required' => false, 'label' => "intervenant", 'class' => Intervenant::class, 'choice_label' => function (Intervenant $intervenant) {
+                return $intervenant->getUser()->getFirstname() . ' ' . $intervenant->getUser()->getName();
+            }]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
