b4abe17635aafdc83f2437061b3fa4b886135d27
Bastien PIPEREL <bastienpiperel@yahoo.fr>
2021-09-08 16:47:19 +0200

    form student is available ! not the edit one though.

--- src/Entity/Student.php
+++ src/Entity/Student.php
@@ -17,8 +17,275 @@ class Student
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $blackListed;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $gender;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $nativeCountry;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $nationality;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $usualLanguage;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $nativeLanguage;
+
+    /**
+     * @ORM\Column(type="string", length=50, nullable=true)
+     */
+    private $phone;
+
+    /**
+     * @ORM\Column(type="string", length=50, nullable=true)
+     */
+    private $mobilePhone;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $birthday;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $tcf;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $status;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getEligibility(): ?bool
+    {
+        return $this->eligibility;
+    }
+
+    public function setEligibility(bool $eligibility): self
+    {
+        $this->eligibility = $eligibility;
+
+        return $this;
+    }
+
+    public function getBlackListed(): ?bool
+    {
+        return $this->blackListed;
+    }
+
+    public function setBlackListed(bool $blackListed): self
+    {
+        $this->blackListed = $blackListed;
+
+        return $this;
+    }
+
+    public function getGender(): ?string
+    {
+        return $this->gender;
+    }
+
+    public function setGender(string $gender): self
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getNativeCountry(): ?string
+    {
+        return $this->nativeCountry;
+    }
+
+    public function setNativeCountry(string $nativeCountry): self
+    {
+        $this->nativeCountry = $nativeCountry;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getNationality(): ?string
+    {
+        return $this->nationality;
+    }
+
+    public function setNationality(string $nationality): self
+    {
+        $this->nationality = $nationality;
+
+        return $this;
+    }
+
+    public function getUsualLanguage(): ?string
+    {
+        return $this->usualLanguage;
+    }
+
+    public function setUsualLanguage(string $usualLanguage): self
+    {
+        $this->usualLanguage = $usualLanguage;
+
+        return $this;
+    }
+
+    public function getNativeLanguage(): ?string
+    {
+        return $this->nativeLanguage;
+    }
+
+    public function setNativeLanguage(string $nativeLanguage): self
+    {
+        $this->nativeLanguage = $nativeLanguage;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getMobilePhone(): ?string
+    {
+        return $this->mobilePhone;
+    }
+
+    public function setMobilePhone(?string $mobilePhone): self
+    {
+        $this->mobilePhone = $mobilePhone;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getBirthday(): ?\DateTimeInterface
+    {
+        return $this->birthday;
+    }
+
+    public function setBirthday(\DateTimeInterface $birthday): self
+    {
+        $this->birthday = $birthday;
+
+        return $this;
+    }
+
+    public function getTcf(): ?string
+    {
+        return $this->tcf;
+    }
+
+    public function setTcf(string $tcf): self
+    {
+        $this->tcf = $tcf;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
     }
 }
---
+++ src/Form/StudentType.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Student;
+use App\Entity\TypeEvent;
+use App\Repository\TypeEventRepository;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\LanguageType;
+
+class StudentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('gender', ChoiceType::class, ['label' => "civilité",'choices' =>['Homme' => "H", 'Femme' => "F", "Autre" => "O" ],
+        'expanded' => true,
+        'multiple' => false,
+    ])
+        ->add('name', TextType::class, ['label' => "nom", 'attr'=>["placeholder"=> "nom"]])
+        ->add('firstname', TextType::class,['label' => "prénom", 'attr'=>["placeholder"=> "prenom"]])
+        ->add('nativeCountry', CountryType::class,['label' => "Pays de naissance", 'attr'=>["placeholder"=> "Pays de naissance"]])
+        ->add('nationality', TextType::class,['label' => "Nationalité", 'attr'=>["placeholder"=> "nationality"]])
+        ->add('usualLanguage', LanguageType::class,['label' => "language d'usage", 'attr'=>["placeholder"=> "Langue d'usage"]])
+        ->add('nativeLanguage', LanguageType::class,['label' => "langue native", 'attr'=>["placeholder"=> "Langue native"]])
+            ->add('phone', TelType::class)
+            ->add('mobilePhone', TelType::class)
+            ->add('email', EmailType::class, ['label' => "email", 'attr'=>["placeholder"=> "email"]])
+            ->add('birthday', DateType::class)
+            ->add('tcf', EntityType::class, ['label' => "diplome désirée", 'mapped'=>false, 'class'=>TypeEvent::class, 'choice_label' => 'title',
+            'query_builder' => function (TypeEventRepository $TypeEventRepository){
+                return $TypeEventRepository->createQueryBuilder('test')
+                ->where('test.type = :value')
+                ->setParameter('value', 'examens');
+            },
+           
+            'choice_value'=> 'title'
+            ])
+            ->add('status', CheckboxType::class, ['label'=>'formation désirée?', "mapped"=>"false"])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Student::class,
+        ]);
+    }
+}
